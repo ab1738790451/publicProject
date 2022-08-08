a0a09a336838384d2733f87c7bc119a135427c07
haibo.liu <1738790451@qq.com>
2022-08-08 18:11:35 +0800

    ip解析更改

---
+++ demo/src/main/java/com/example/demo/common/Ip2regionLoader.java
@@ -0,0 +1,11 @@
+package com.example.demo.common;
+
+/**
+ * @Author: liuhaibo
+ * @Company: 优积谷
+ * @Date: 2022/8/8 16:43
+ * @Version: 1.0.0
+ * @Description: 描述
+ */
+public class Ip2regionLoader {
+}
